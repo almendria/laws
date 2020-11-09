490a7b85502362464818cb64916001c3a6a54b1d
Timothy Schoonover <t.sch.key@gmail.com>
2020-11-09 11:02:04 -0800

    Create constitution.ml

---
+++ constitution.ml
@@ -0,0 +1,271 @@
+CONSTITUTION ACT
+
+The workers and people of the republics of the United Socialist Republics do solemnly recognize that–
+Whereas: The people of our nation and of our planet have for generations been oppressed by the evils of capitalism and economic inequalities have caused ever-growing inequality;
+Whereas: The unity of our country and people is dependent on a structured and egalitarian government;
+Whereas: By uniting under the United Socialist Republics, the workers and the people do hereby–
+Resolve:
+(1) To establish this Constitution of the United Socialist Republics Union, in order to:
+Establish the existence of the nation of the republics of the United Socialist Republics, and its rightful government, its federalist government;
+(2) Outline the structure, function, and powers of that nation’s government;
+(3) Create the executive system, that shall enact and enforce all laws;
+(4) Create the legislative system, that is the basis of the citizens’ power over that government;
+(5) Create the judicial system, established to interpret and apply the laws created by the legislative system;
+(6) Enable the creation of ministries to enforce the law.
+
+# §1. Foundations
+
+¶1. The name of this act is the Constitution Act.
+
+¶2.The people do hereby establish and found the United Socialist Republics (“Government”) which shall have the power of enforcement and jurisdiction over this act.
+
+¶3. The Government shall have ownership over all private property within its jurisdiction, and the Government shall be a cooperative of the citizens, and its ownership shall be shared between all citizens evenly.
+
+¶4. The United Socialist Republics shall be a socialist nation following Marxism and shall be under the administration of the Communist Party of the United Socialist Republic (“Communist Party”).
+
+# §2. Legislative
+
+¶1. The National Assembly shall exist as the federal unicameral parliament of the United Socialist Republics and its jurisdiction shall exist throughout the entire nation.
+
+
+¶2.
+Every citizen shall be a member of the National Assembly.
+
+
+¶3.
+The National Assembly has general jurisdiction in all decisions that are not restricted by this Constitution.
+
+
+¶4.
+There shall exist three types of proposals to the National Assembly–
+Acts, which relate to creating, amending, or deleting Acts;
+Referendums, which relate to creating, amending, or deleting federal jurisdictions, creating a new Constitution or amending or deleting this Constitution;
+Decrees, which relate to informal direct action interpreted as law with no accompanying act.
+
+
+
+¶5.
+The National Assembly shall have general jurisdiction over the executive branch and may create Ministries and other bodies to enforce its laws.
+
+
+¶6.
+The primary executive ministries shall include:
+Ministry of Agriculture;
+Ministry of the Economy;
+Ministry of Education;
+Ministry of Energy;
+Ministry of Foreign Affairs;
+Ministry of Government Integrity;
+Ministry of Health;
+Ministry of Housing;
+Ministry of Justice;
+Ministry of the National Bank;
+Ministry of Security;
+Ministry of Transporation;
+Ministry of Trade;
+Ministry of War.
+
+§3.
+For
+Judicial–
+
+
+¶1.
+There shall exist the Supreme Court of the United Socialist Republics (“Supreme Court”), which shall be the first and primary national court of the United Socialist Republics.
+
+
+¶2.
+The National Assembly may create by decree inferior courts to the Supreme Court.
+
+
+¶3.
+Justices and Judges will be appointed by the Council of the Ministry of Justice, whose 11 members are elected by the National Assembly.
+
+
+¶4.
+Justices and Judges may be impeached by a simple majority vote of the National Assembly in their jurisdiction.
+
+
+¶5.
+All court cases will be open to the public.
+
+
+¶6.
+In all cases, the Government, acting through the court, shall provide an attorney on the behalf of the defendant and prosecution for their representation and defense.
+
+
+¶7.
+General cases will originate within Lower Courts (lowest level courts);
+Cases in Lower Courts have the right to be appealed to Appellate Courts (middle level courts);
+Appellate Courts have the option of upholding the decision of Lower Courts, disagreeing fully, or disagreeing by recommendation, in which the case is sent to the Lower Court with the opinion of the Appellate Court;
+Cases in Appellate Courts can apply to be appealed to the Supreme Court with a writ of certiorari.
+The Supreme Court has the right to choose to accept or decline cases.
+Non-general cases, including cases between the different Ministries, Ministers, government administrative levels, and foreign governments will begin in the Supreme Court.
+
+
+
+¶8.
+Defendants are always innocent in the view of the Court until definitively proven otherwise.
+
+
+¶9.
+This Constitution should be interpreted by its text as it means, without regard for the connotation of text or modern interpretation, meaning that it should be interpreted with a textualist approach.
+§4.
+For
+Autonomous Republics–
+
+
+¶1.
+Autonomous Republics may be brought into the United Socialist Republics from time to time by the National Assembly.
+
+
+¶2.
+Autonomous Republics may issue constituting charters which shall allow them to not be subject to the federal laws of the United Socialist Republics. By default, all federal laws and authority will exist on Autonomous Republics unless otherwise stated by their constituting charter.
+
+
+¶3.
+Autonomous Republics must follow and recognize this Constitution.
+§6.
+For
+Rights–
+
+
+¶1.
+Everyone has the right to life from birth.
+
+
+¶2.
+No one shall be subjected to cruel, inhumane, or degrading torture, treatment, or punishment.
+
+
+¶3.
+Everyone has the right to criticize all forms of ideas, or ideologies, whether it may be offensive or not.
+
+
+¶4.
+Everyone below the age of 17 must not directly serve in armed conflict unless the individual decides to serve on armed conflict.
+
+
+¶5.
+No one shall be subjected to arbitrary arrest, detention, or exile.
+
+
+¶6.
+Everyone is entitled to a fair and public hearing to determine whether they are guilty of the criminal charges against them.
+
+
+¶7.
+Everyone charged with an offense has the right to be presumed innocent until proven guilty according to law.
+
+
+¶8.
+Everyone has the right to leave any country, including their own.
+
+
+¶9.
+Everyone has the right to apply for citizenship.
+
+
+¶10.
+Two people of mature age have the right to found a family and with consent, engage in sexual activities.
+
+
+¶11.
+No person above adult age besides those older by less than 3 years of the mature aged person should form any intimate relationships with them.
+
+
+¶12.
+Everyone has the right to own personal property alone as well as in association with others, which shall not be taken from them by others or the government without their consent.
+
+
+¶13.
+Everyone has the right to organize and form a peaceful assembly to protest whatever they desire.
+
+
+¶14.
+Everyone has the right, without any discrimination to:
+Work;
+Choice of employment (except for where is absolutely necessary for the protection of employees);
+Just and favorable conditions of work;
+Equal pay for equal work;
+Form and join trade unions for the protection of the worker;
+Just provisions of housing, or housing loans, and loan rates reflective of their risk to the creditors;
+Just and equal usage of public accommodations and areas limited to a reasonable extent.
+
+
+
+¶15.
+Everyone has the right to rest and leisure, including reasonable limitation of working hours and periodic holidays.
+
+
+¶16.
+Everyone has the right to food, water, and shelter.
+
+
+¶17.
+Everyone has the right to education. Fundamental education shall be compulsory.
+
+
+¶18.
+Everyone has the right freely to participate in the cultural life of the community, to enjoy the arts and to share in scientific and technological advancement and its benefits.
+
+
+¶19.
+Everyone has the right to access infrastructure within reasonable means.
+
+
+¶20.
+Everyone has the right to enjoy and entertain themselves, within the reasonable limits of the law.
+
+
+¶21.
+Everyone has the right to high quality, free healthcare. No person will have to declare bankruptcy or harm their financial standing in order to pay for any part of their healthcare arrangement, within reasonable limitations.
+
+
+¶22.
+Everyone has the right to be equal before the law and therefore no laws shall be discriminatory.
+
+
+¶23.
+Everyone has the right to an attorney in a court of law.
+
+
+¶24.
+Everyone has the right to stay free inside their personal property: law enforcement must obtain a warrant from the court with an oath of affirmation and probable cause before they are given authorization to enter personal property, excluding privately owned public spaces within the reasonable limits of the law and the consent of the property owner.
+
+
+¶25.
+Everyone has the right to trial by jury.
+
+
+¶26.
+Everyone has the right to not be put subjected to cruel punishments.
+
+
+¶27.
+Everyone has the right to own and bear firearms, except for disturbed and mentally ill persons as determined by a doctor, or minors. Sellers have the right to not sell to any person for any reason excluding discrimination as determined by law.
+
+
+¶28.
+In this section, “everyone” shall not include those that have been convicted of crimes and are on sentences or rights in this act contradict certain provisions of their release, excluding rights pertaining specifically to the judicial system and equality above the law.
+§7.
+For
+Amendments–
+
+
+¶1.
+Amendments to Act can be made through the standard referendum process in the National Assembly.
+
+
+¶2.
+Amendments can be made directly to the text of this Act once approved.
+
+
+¶3.
+A majority of votes in the National Assembly are required for amendments to this Constitution.
+
+
+¶4.
+This Constitution is supreme to any other law.
+
+Approved by the popular vote of the National Assembly in a referendum.
